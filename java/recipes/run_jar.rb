22ce7e8b9a648148fe2a4d2f172316270ce46eb0
brian@Z97X <brian@jongensvantechniek.nl>
2016-06-02 00:49:21 +0200

    Fix setting environment variable for a user (by Steven's suggestion)

--- java/recipes/run_jar.rb
+++ java/recipes/run_jar.rb
@@ -8,22 +8,24 @@ node[:deploy].each do |application, deploy|
         next
     end
 
+    ## TODO UNSET ENVIRONMENT VARIABLE WITH SAME KEY DIFFERENT VARIABLE AND LAUNCH WARNING IN LOG
+
     node[:custom_env][application.to_s]["environment_variables"].each do |key, variable|
-        execute "set environment variable" do
-            user    "root"
-            command "echo #{key}=#{variable} >> /etc/environment"
-            not_if "cat /etc/environment | grep #{key}=#{variable}"
+        execute "set_environment_variable" do
+            user    "#{deploy[:user]}"
+            command "cd && echo 'export #{key}=#{variable}' >> .profile"
+            not_if  "cat .profile | grep #{key}=#{variable}"
         end
     end
 
-    execute "stop java jar" do
+    execute "stop_jar" do
         user    "#{deploy[:user]}"
         command "screen -S #{application.to_s} -X quit"
         only_if "screen -ls | grep #{application.to_s}"
     end
 
-    execute "start java jar" do
-        user "#{deploy[:user]}"
+    execute "run_jar" do
+        user    "#{deploy[:user]}"
         cwd     "#{deploy[:deploy_to]}/current"
         command "screen -dmS #{application.to_s} java -jar #{node[:custom_env][application.to_s]['jar']}"
     end
