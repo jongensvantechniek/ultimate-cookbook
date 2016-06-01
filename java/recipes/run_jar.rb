f9677a32716e615c44b2d39aab1c9cb5e0722a46
brian@Z97X <brian@jongensvantechniek.nl>
2016-06-02 01:04:26 +0200

    Actually verified whether or not the environment variable is set properly

--- java/recipes/run_jar.rb
+++ java/recipes/run_jar.rb
@@ -14,8 +14,8 @@ node[:deploy].each do |application, deploy|
         execute "set_environment_variable" do
             user    "#{deploy[:user]}"
             cwd     "/home/#{deploy[:user]}"
-            command "echo 'export #{key}=#{variable}' >> .profile"
-            not_if  "cat .profile | grep #{key}=#{variable}"
+            command "echo 'export #{key}=#{variable}' >> .bashrc"
+            not_if  "cat .bashrc | grep #{key}=#{variable}"
         end
     end
 
