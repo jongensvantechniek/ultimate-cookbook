19879566625e2fc41d17523f35d771e3a7a06df6
Peter Verkooijen <peter@peershaped.nl>
2018-02-12 17:18:28 +0100

    Remove srvc/www mentions from acl_setup.rb

--- symfony/recipes/acl_setup.rb
+++ symfony/recipes/acl_setup.rb
@@ -4,7 +4,5 @@ script "acl_setup" do
   cwd "/"
   code <<-EOH
   apt-get install acl
-  echo "UUID=07aebd28-24e3-cf19-e37d-1af9a23a45d4    /srv/www    ext4   defaults,acl   0   2" >> /etc/fstab
-  mount -o remount,acl /srv/www
   EOH
 end
